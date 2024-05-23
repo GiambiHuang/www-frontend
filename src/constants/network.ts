a8a087e3ab327cf656118bf8c777c344687dea1d
Giambi <nash9328@hotmail.com>
2024-05-23 22:32:30 +0800

    feat: new program id

--- src/constants/network.ts
+++ src/constants/network.ts
@@ -9,6 +9,6 @@ export const endpoint = 'https://testnet.dev2.eclipsenetwork.xyz/';
 
 // export const programId = 'DKFmaoLpqpc6TFyNwQBRXZhYSqbFctaJqFn9z3xha9ue';
 
-export const programId = 'HvsagFEeAjfGtBY7iR8ekcrFk2uQx4dPSnuJ5mj76CLt';
+export const programId = 'DSjRv8ywMv2UaiaFiRB7Yp3AnAS7HHCueu8fvvJH3YS2';
 // export const gamePublicKey = new web3.PublicKey('GArUSk4zCdmZJsx9bVF8vTawnJuwbemmSZ11PX2pLAru');
-export const gameMatchPublicKey = new web3.PublicKey('CF2EDUoUeoewdxXtZn4QaNDNQe4LJ3jkEZi5U21xRMZE');
+export const gameMatchPublicKey = new web3.PublicKey('CwGCZWkBbJ8KDddNbCDKRbwawibghaSYZ2QHYBnxuVKw');
